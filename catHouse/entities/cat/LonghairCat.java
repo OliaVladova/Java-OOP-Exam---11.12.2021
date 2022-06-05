ae34b5083185640283b1489de0ec9d3555fa181d
OliaVladova <ovladova1@gmail.com>
2022-06-05 23:06:43 +0300

    Add files via upload

---
+++ catHouse/entities/cat/LonghairCat.java
@@ -0,0 +1,14 @@
+package catHouse.entities.cat;
+
+public class LonghairCat extends BaseCat {
+    private static  final int KILOGRAMS = 9;
+
+    public LonghairCat(String name, String breed, double price) {
+        super(name, breed, price);
+    }
+
+    @Override
+    public void eating() {
+        this.setKilograms(this.KILOGRAMS+3);
+    }
+}
